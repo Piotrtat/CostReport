11324850990804a9d0e0218e6b9f2a31bc06bfc1
Piotrtat <44445543+Piotrtat@users.noreply.github.com>
2018-12-20 22:02:04 +0100

    Add files via upload

--- glowny_dziennik.py
+++ glowny_dziennik.py
@@ -7,5 +7,20 @@ class DziennikWindow(QtWidgets.QMainWindow,Ui_Dziennik):
         super().__init__()
         self.setupUi(self)
         self.show()
+        self.showMaximized()
+        self.setWindowTitle("Moj Dziennik")
+        self.pushButton.clicked.connect(self.koszt_podrozy)
+
+    def koszt_podrozy(self):
+        odleglosc = self.lineEdit_Odleglosc.text()
+        print(odleglosc)
+        cena_za_litr = self.lineEdit_Cena_za_litr.text()
+        print(cena_za_litr)
+        spalanie = self.lineEdit_Spalanie.text()
+        print(spalanie )
+
+
+
+
 
 
