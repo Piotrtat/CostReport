4ddbe9c22aea26a0bb5682e94b7ee4a97a8299d5
Piotrtat <44445543+Piotrtat@users.noreply.github.com>
2018-12-29 15:50:18 +0100

    Add files via upload

--- glowny_dziennik.py
+++ glowny_dziennik.py
@@ -2,16 +2,28 @@ from PyQt5 import QtWidgets
 
 from ui_dziennik_kosztow import Ui_Dziennik
 
+from PyQt5 import QtGui
+
+import ui_dziennik_kosztow
+
+import sys
+
+
+
 class DziennikWindow(QtWidgets.QMainWindow,Ui_Dziennik):
     def __init__(self):
+        global odleglosc
         super().__init__()
         self.setupUi(self)
         self.show()
         self.showMaximized()
         self.setWindowTitle("Moj Dziennik")
         self.pushButton.clicked.connect(self.koszt_podrozy)
+        self.pushButton.clicked.connect(lambda: self.koszt_podrozy())
+
 
     def koszt_podrozy(self):
+        global wynik
         odleglosc = self.lineEdit_Odleglosc.text()
         self.odleglosc = float
         print(odleglosc)
@@ -21,9 +33,17 @@ class DziennikWindow(QtWidgets.QMainWindow,Ui_Dziennik):
         spalanie = self.lineEdit_Spalanie.text()
         self.spalanie = float
         print(spalanie)
-        wynik = self.lineEdit_Wynik.text()
         wynik = float(odleglosc)/100 * (float(cena_za_litr) * float(spalanie))
         print(wynik)
+        self.lineEdit_Wynik.setText(str(wynik))
+
+
+
+
+
+
+
+
 
 
 
