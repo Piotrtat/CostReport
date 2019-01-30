deaa39f479cc87577fa79ef8c9ad985083287670
Piotrtat <44445543+Piotrtat@users.noreply.github.com>
2019-01-30 20:13:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from glowny_dziennik import DziennikWindow
+import time
+import threading
+
 
 app = QApplication(sys.argv)
 
 dziennik = DziennikWindow()
+
+def my_timer(cost_report):
+    while True:
+        cost_report.clock_2()
+        time.sleep(1)
+
+my_thread = threading.Thread(target=my_timer, args=[dziennik, ])
+my_thread.start()
+
+
 
 sys.exit(app.exec_())
